5c995f670a24737f72d6936b5481f5b603eb0019
maxdegers <maxdegers@gmail.com>
2024-05-29 13:46:59 +0200

    adeded

--- srcs/main/main_loop.c
+++ srcs/main/main_loop.c
@@ -6,7 +6,7 @@
 /*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 11:30:10 by mbrousse          #+#    #+#             */
-/*   Updated: 2024/05/24 12:13:06 by mbrousse         ###   ########.fr       */
+/*   Updated: 2024/05/29 13:46:46 by mbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int	key_p(int key, t_data *data)
 
 int	main_loop(t_data *data, t_mlx *cub)
 {
+	// mlx_loop_hook(cub->mlx, ->>add_fonction_qui_gen<<-, data);
 	mlx_hook(cub->win, 17, 1L << 0, close_window, data->g);
 	mlx_hook(cub->win, 2, 1L << 0, key_p, data);
 	mlx_loop(cub->mlx);
