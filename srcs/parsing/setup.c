5d9429dc97aabbcfeab2a6d00e937a6df10c9ffb
maxdegers <maxdegers@gmail.com>
2024-06-03 13:32:13 +0200

    adeded

--- srcs/parsing/setup.c
+++ srcs/parsing/setup.c
@@ -6,7 +6,7 @@
 /*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 14:52:17 by mbrousse          #+#    #+#             */
-/*   Updated: 2024/06/03 11:16:16 by mbrousse         ###   ########.fr       */
+/*   Updated: 2024/06/03 13:30:48 by mbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	setup_map(t_map *map)
 	map->map = NULL;
 	map->window.x = WIDTH;
 	map->window.y = HEIGHT;
-	map->zoom = 2;
+	map->zoom = 5;
 }
 
 void	ft_setup_map(t_data *data)
